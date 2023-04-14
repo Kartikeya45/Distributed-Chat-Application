d7389d57148df671fe1d1b40f168d6fbaf62f1f9
SBhat2615 <siddharthabhat09@gmail.com>
2023-04-14 12:29:58 +0530

    server

--- backend/app/server.js
+++ backend/app/server.js
@@ -1,7 +1,21 @@
 // npm run devStart, to start application
 
 const express = require('express')
-const app = express()
+const socketio = require("socket.io")
+const http = require("http")
+const redis = require('redis')
+const cors = require('cors')
+
+
+app = express()
+const httpServer = http.createServer(app)
+const io = socketio(httpServer, {
+  cors : {
+    origin : "*",
+  }
+})
+
+
 
 const port = 3000
 const serveChat = require('./routes/serveChats')
