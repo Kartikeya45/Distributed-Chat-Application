844eda7d61a1cc71d33e884ccc55e2eeb7978e40
EnergeticQuanta17 <preetham.trh@gmail.com>
2023-04-21 15:50:07 +0530

    from lab - using git access token

---
+++ backend/ds_features/LoadBalancer/recv.py
@@ -0,0 +1,14 @@
+import sys
+import zmq
+
+port = "5556"
+context = zmq.Context()
+socket = context.socket(zmq.SUB)
+socket.connect('tcp://127.0.0.1:5556')
+socket.setsockopt_string(zmq.SUBSCRIBE, "")
+
+while(True):
+    message = socket.recv_string()
+    print("Received message: ", message)
+    if(message=='close'):
+        break
---
+++ backend/ds_features/LoadBalancer/sender.py
@@ -0,0 +1,12 @@
+import zmq
+import random
+import sys
+import time
+
+context = zmq.Context()
+socket = context.socket(zmq.PUB)
+socket.bind("tcp://127.0.0.1:5556")
+
+while True:
+    message = input("Enter message to be sent: ")
+    socket.send_string(message)
